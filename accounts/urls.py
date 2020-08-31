5b9b2b8adfd97fd181e20f4358efdbd3fa7e5928
Husnain Ahmad <husnainahmad521@gmail.com>
2020-08-31 15:22:07 +0500

    initial project

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import ( 
+    home,
+    customer, 
+    products
+    )
+
+urlpatterns = [
+    path('', home),
+    path('customer/', customer),
+    path('products/', products),
+
+] 
