dea7860510514ed140734bf85985bc0fcc5acbf9
Husnain Ahmad <husnainahmad521@gmail.com>
2020-09-02 17:12:03 +0500

    login insted of singin

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('create_order/<str:pk>',create_order, name="create_order"),
     path('update_order/<str:pk>',update_order, name="update_order"),
     path('delete_order/<str:pk>',delete_order, name="delete_order"),
-    path('signin/',signin ,name='signin'),
+    path('login/',signin ,name='signin'),
     path('register/' , register , name="register"),
 
 
